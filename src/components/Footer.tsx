31547ff6b44f548e20440338743d772640871ac4
Erik Karas <Erik.Karas@email.cz>
2024-12-03 21:48:55 +0100

    Odstraněn nepoužitý LinkedInIcon import

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import GitHubIcon from '@mui/icons-material/GitHub';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 import '../assets/styles/Footer.scss';
 
 function Footer() {
@@ -10,9 +10,7 @@ function Footer() {
         <a href="https://github.com/ErikKaras" target="_blank" rel="noreferrer">
           <GitHubIcon />
         </a>
-        <a href="https://www.linkedin.com/in/erikkaras" target="_blank" rel="noreferrer">
-          <LinkedInIcon />
-        </a>
+
       </div>
       <p>© Copyright 2024 Erik Karas</p>
     </footer>
